d1a3580c3c768bea3f2a9251a7e48e63608001cf
Erik Goesmann <erikgoesmann@gmail.com>
2018-08-13 10:31:22 +1200

    Version 1
    Created framework. Initiated tkinter and window.

---
+++ crazy_comics.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+
+root = Tk()
+root.title("Tickets label")
+root.geometry('800x1000')
+
+
+root.mainloop()
--- tickets.py
+++ tickets.py
@@ -3,7 +3,17 @@ from tkinter import *
 def update_label():
     ticket_info.set("")
     for p in tickets:
-        ticket_info.set(ticket_info.get() + p._name + " $" + str(p._price))
+        ticket_info.set(ticket_info.get() + p._name + " $" + str(p._price) + " " + str(p._amount) + "\n")
+
+def change_stuff():
+    for i in tickets:
+        if selected_ticket.get() == i._name:
+            i._amount -= int(num_tickets.get())
+            tickets_sold.set(int(tickets_sold.get()) + int(num_tickets.get()))
+        print(i._name)
+        print(i._price)
+
+    update_label()
 
 class Tickets:
     def __init__(self, name, price, amount):
@@ -30,8 +40,30 @@ ticket_lbl = Label(root, textvariable=ticket_info)
 ticket_lbl.grid(row=0)
 
 selected_ticket = StringVar()
-selected_ticket.set(tickets[0])
+selected_ticket.set(ticket_names[0])
 
+ticket_menu = OptionMenu(root, selected_ticket, *ticket_names)
+ticket_menu.grid(row=1)
+
+num_tickets = StringVar()
+
+ticket_entry = Entry(root, textvariable=num_tickets)
+ticket_entry.grid(row=2)
+
+select_btn = Button(root, text="Select", command=change_stuff)
+select_btn.grid(row=3)
+
+tickets_sold = StringVar()
+tickets_sold.set(0)
+
+sold_lbl = Label(root, textvariable = tickets_sold)
+sold_lbl.grid(row=4)
+
+tickets_total = StringVar()
+tickets_total.set("$0")
+
+dollar_lbl = Label(root, textvariable = tickets_total)
+dollar_lbl.grid(row=5)
 
 update_label()
 root.mainloop()
