01df60befa9a101883c0a78036ecd79445847a0c
Erik Goesmann <erikgoesmann@gmail.com>
2018-08-14 11:09:18 +1200

    update_label and Comics class
    created an update_label function and added a class for comics and created a list for each class.

--- crazy_comics.py
+++ crazy_comics.py
@@ -1,12 +1,24 @@
 from tkinter import *
 
-#def update_label():
-    #ticket_info.set("")
-    #for p in tickets:
-        #ticket_info.set(ticket_info.get() + p._name + " $" + str(p._price) + " " + str(p._amount) + "\n")
+def update_label():
+    comic_info.set("")
+    for p in comics:
+        comic_info.set(comic_info.get() + p._name + " $" + str(p._price) + " " + str(p._amount) + "\n")
+
+class Comics:
+    def __init__(self, name, amount):
+        self._name = name 
+        self._amount = amount
+        comics.append(self)
+        comic_names.append(self._name)
+
+comics = []
+comic_names = []
+
+
 
 root = Tk()
-root.title("Tickets label")
+root.title("comics label")
 root.geometry('800x1000')
 
 frame1 = Frame(root, relief = "groove", borderwidth = 2, width = 800).grid(row = 0, column = 0, sticky = N+S+W+E)
@@ -16,10 +28,11 @@ frame4 = Frame(root, relief = "groove", borderwidth = 2, width = 400).grid(row =
 frame5 = Frame(root, relief = "groove", borderwidth = 2, width = 400).grid(row = 2, column = 1, sticky = N+S+W+E)
 
 
-ticket_info = StringVar()
 
-ticket_lbl = Label(root, textvariable=ticket_info)
-ticket_lbl.grid(row=0)
+comic_info = StringVar()
 
-#update_label()
+comic_lbl = Label(frame1, textvariable=comic_info)
+comic_lbl.grid(row=0)
+
+update_label()
 root.mainloop()
