0c5bf19c19042a9dccc7c435ba149009d878424a
Erik Goesmann <erikgoesmann@gmail.com>
2018-08-14 11:02:53 +1200

    Frames Added
    Added 5 frames to add content into

--- crazy_comics.py
+++ crazy_comics.py
@@ -1,9 +1,25 @@
 from tkinter import *
 
+#def update_label():
+    #ticket_info.set("")
+    #for p in tickets:
+        #ticket_info.set(ticket_info.get() + p._name + " $" + str(p._price) + " " + str(p._amount) + "\n")
 
 root = Tk()
 root.title("Tickets label")
 root.geometry('800x1000')
 
+frame1 = Frame(root, relief = "groove", borderwidth = 2, width = 800).grid(row = 0, column = 0, sticky = N+S+W+E)
+frame2 = Frame(root, relief = "groove", borderwidth = 2, width = 400).grid(row = 1, column = 0, sticky = N+S+W+E)
+frame3 = Frame(root, relief = "groove", borderwidth = 2, width = 400).grid(row = 1, column = 1, sticky = N+S+W+E)
+frame4 = Frame(root, relief = "groove", borderwidth = 2, width = 400).grid(row = 2, column = 0, sticky = N+S+W+E)
+frame5 = Frame(root, relief = "groove", borderwidth = 2, width = 400).grid(row = 2, column = 1, sticky = N+S+W+E)
 
+
+ticket_info = StringVar()
+
+ticket_lbl = Label(root, textvariable=ticket_info)
+ticket_lbl.grid(row=0)
+
+#update_label()
 root.mainloop()
